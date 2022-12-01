f687579b17260b8cec55d6e9028b18b3775dafb2
하이디_김초원 <heidi.cho@kakaostyle.com>
2022-12-01 18:07:46 +0900

    create catalog-ux project

---
+++ src/test/kotlin/com/croquis/catalogux/CatalogUxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.croquis.catalogux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatalogUxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
