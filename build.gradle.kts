15a53e9a9db581570e1404e54f37e52795fb49b6
하이디_김초원 <heidi.cho@kakaostyle.com>
2022-12-01 19:05:21 +0900

    build.gradle 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,57 +1,123 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
-plugins {
-	id("org.springframework.boot") version "2.7.6"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
-	id("org.springframework.experimental.aot") version "0.12.1"
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.7.20"))
+    }
 }
 
 group = "com.croquis"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
 
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+plugins {
+    val kotlinVersion = "1.7.20"
+    idea
+    jacoco
+    kotlin("jvm") version kotlinVersion
+    kotlin("kapt") version kotlinVersion apply false
+    kotlin("plugin.spring") version kotlinVersion apply false
+    id("org.springframework.boot") version "2.5.3" apply false
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("com.avast.gradle.docker-compose") version "0.16.8"
+    id("org.jlleitschuh.gradle.ktlint") version "10.1.0" apply false
+    id("org.jlleitschuh.gradle.ktlint-idea") version "10.1.0"
 }
 
-repositories {
-	maven { url = uri("https://repo.spring.io/release") }
-	mavenCentral()
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    repositories {
+        mavenCentral()
+    }
+
+    ktlint {
+        filter {
+            exclude("*.kts")
+            exclude("**/generated/**")
+        }
+    }
 }
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-graphql")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
-	testImplementation("org.springframework.graphql:spring-graphql-test")
+tasks {
+    register<Exec>("lint") {
+        commandLine = "./gradlew ktlintCheck".split(" ")
+    }
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
-}
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "jacoco")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
 
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+    java.sourceCompatibility = JavaVersion.VERSION_11
 
-tasks.withType<BootBuildImage> {
-	builder = "paketobuildpacks/builder:tiny"
-	environment = mapOf("BP_NATIVE_IMAGE" to "true")
+    val kotestVersion = "4.6.0"
+
+    dependencies {
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.kotest:kotest-runner-junit5:${kotestVersion}")
+        testImplementation("io.kotest:kotest-assertions-core:${kotestVersion}")
+        testImplementation("io.kotest:kotest-property:${kotestVersion}")
+        testImplementation("io.kotest:kotest-framework-datatest:${kotestVersion}")
+        testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
+        testImplementation("io.mockk:mockk:1.12.1")
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    jacoco.toolVersion = "0.8.8"
+
+    tasks.test {
+        extensions.configure(JacocoTaskExtension::class) {
+            val destinationFile = file("$buildDir/jacoco/jacoco.exec")
+            setDestinationFile(destinationFile)
+        }
+        finalizedBy("jacocoTestReport")
+    }
+
+    tasks.jacocoTestReport {
+        reports {
+            html.required.set(true)
+            xml.required.set(true)
+            xml.outputLocation.set(file("$buildDir/jacoco/jacoco.xml"))
+            finalizedBy("jacocoTestCoverageVerification")
+        }
+    }
+
+    tasks.jacocoTestCoverageVerification {
+        violationRules {
+            rule {
+                element = "CLASS"
+
+                limit {
+                    counter = "BRANCH"
+                    value = "COVEREDRATIO"
+                }
+            }
+        }
+    }
+
+    tasks.jacocoTestReport {
+        reports {
+            html.required.set(true)
+            xml.required.set(true)
+        }
+    }
+    tasks.test {
+        finalizedBy("jacocoTestReport")
+    }
 }
